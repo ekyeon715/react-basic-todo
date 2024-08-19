188e2fe68bcd3b811408f245e9f108426d592cad
ekyeon715 <rkdwldus9636@naver.com>
2024-08-19 15:24:50 +0900

    feat: 투두리스트 완료상태 업데이트 추가

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 const SAMPLE_TODOS = [
-  { id: 1, text: "Buy milk" },
-  { id: 2, text: "Clean the house" },
-  { id: 3, text: "Go for a run" },
-  { id: 4, text: "Finish homework" },
-  { id: 5, text: "Call mom" },
-  { id: 6, text: "Buy groceries" },
-  { id: 7, text: "Walk the dog" },
-  { id: 8, text: "Read a book" },
-  { id: 9, text: "Do laundry" },
-  { id: 10, text: "Write code" },
+  { id: 1, text: "Buy milk", completed: false },
+  { id: 2, text: "Clean the house", completed: false },
+  { id: 3, text: "Go for a run", completed: false },
+  { id: 4, text: "Finish homework", completed: false },
+  { id: 5, text: "Call mom", completed: false },
+  { id: 6, text: "Buy groceries", completed: false },
+  { id: 7, text: "Walk the dog", completed: false },
+  { id: 8, text: "Read a book", completed: false },
+  { id: 9, text: "Do laundry", completed: false },
+  { id: 10, text: "Write code", completed: false },
 ];
 
 const TodoList = () => {
@@ -24,12 +24,29 @@ const TodoList = () => {
       return;
     }
 
-    setTodos([{ id: crypto.randomUUID(), text: newTodo }, ...todos]);
+    setTodos([
+      { id: crypto.randomUUID(), text: newTodo, completed: false },
+      ...todos,
+    ]);
     setNewTodo("");
   };
 
   const handleInputChange = (event) => {
     setNewTodo(event.target.value);
+  };
+
+  const handleUpdate = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(updateTodos);
   };
 
   return (
@@ -45,7 +62,12 @@ const TodoList = () => {
       </form>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+            <p>
+              {todo.text} - {String(todo.completed)}
+            </p>
+            <button onClick={() => handleUpdate(todo.id)}>완료</button>
+          </li>
         ))}
       </ul>
     </div>
