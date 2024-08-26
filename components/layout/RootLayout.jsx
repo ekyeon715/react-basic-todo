5ba48b29184d0f81254d2c5ec989fbbbb36eac81
ekyeon715 <rkdwldus9636@naver.com>
2024-08-26 19:56:41 +0900

    style: css reset 추가

---
+++ components/layout/RootLayout.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const RootLayout = ({ children }) => {
+  return (
+    <RootLayoutMain>
+      <RootLayoutContents>{children}</RootLayoutContents>
+    </RootLayoutMain>
+  );
+};
+
+export default RootLayout;
+
+const RootLayoutMain = styled.main`
+  background-color: #f7f6f9;
+`;
+
+const RootLayoutContents = styled.div``;
