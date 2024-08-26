3b37551bfb1f2ab8b642c82d47905309b8e2c015
ekyeon715 <rkdwldus9636@naver.com>
2024-08-26 20:44:15 +0900

    style: 대시보드 스타일 추가

---
+++ components/todo/TodoDashboard.jsx
@@ -0,0 +1,86 @@
+import { ClipboardCheck, Ellipsis, Monitor, Video } from "lucide-react";
+import React from "react";
+import styled from "styled-components";
+
+const TodoDashboard = () => {
+  return (
+    <DashboardSection>
+      <DashboardHeader>
+        <h1>Dashboard</h1>
+      </DashboardHeader>
+      <DashboardCardList>
+        <DashboardCard flex="2" color="#e7582b">
+          <div>
+            <ClipboardCheck />
+            <Ellipsis />
+          </div>
+          <p>
+            16 <br /> All tasks
+          </p>
+        </DashboardCard>
+        <DashboardCard flex="1" color="#582be7">
+          <div>
+            <Monitor />
+            <Ellipsis />
+          </div>
+          <p>
+            4 <br />
+            Completed
+          </p>
+        </DashboardCard>
+        <DashboardCard flex="1" color="#242424">
+          <div>
+            <Video />
+            <Ellipsis />
+          </div>
+          <p>
+            2 <br />
+            Pending
+          </p>
+        </DashboardCard>
+      </DashboardCardList>
+    </DashboardSection>
+  );
+};
+
+export default TodoDashboard;
+
+const DashboardSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const DashboardHeader = styled.div`
+  h1 {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+`;
+
+const DashboardCardList = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+const DashboardCard = styled.div`
+  background-color: ${(props) => props.color};
+  padding: 1rem;
+  border-radius: 1rem;
+  height: calc((640px / 4));
+
+  color: white;
+  font-weight: bold;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: ${(props) => props.flex};
+
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import RootLayout from "../components/layout/RootLayout";
 import TodoContainer from "../components/todo/TodoContainer";
+import TodoDashboard from "../components/todo/TodoDashboard";
 
 const App = () => {
   return (
     <RootLayout>
+      <TodoDashboard />
       <TodoContainer />
     </RootLayout>
   );
